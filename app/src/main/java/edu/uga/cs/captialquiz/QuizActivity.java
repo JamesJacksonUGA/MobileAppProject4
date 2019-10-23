e46f64d5a130a7a0e39b59ca551203b7cf72894a
Derrick Kyei <D-Rock@Derricks-MacBook-Pro.local>
2019-10-23 18:40:18 -0400

    quiz table created

--- app/src/main/java/edu/uga/cs/captialquiz/QuizActivity.java
+++ app/src/main/java/edu/uga/cs/captialquiz/QuizActivity.java
@@ -8,10 +8,13 @@ import android.os.Bundle;
 public class QuizActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private QuizFragmentCollectionAdapter adapter;
+    private DatabaseHelper myDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        myDatabase = new DatabaseHelper(this);
+
         viewPager = findViewById(R.id.pager);
         adapter = new QuizFragmentCollectionAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);//view pager activity contains instances of the fragment
